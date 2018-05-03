b2a3a79922f93bb62b3b8bc9b7b05c73801adb70
Tautcius <t.bujauskas@gmail.com>
2018-05-03 21:53:16 +0300

    start

---
+++ PRuta/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <b-img class="logo" :src="require('./assets/prutaLogo.png')" fluid alt="Responsive image" /> 
+    <NavBar />
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import NavBar from './components/NavBar'
+export default {
+  name: 'app',
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+</style>
---
+++ PRuta/src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+   <b-navbar toggleable="md" type="light" variant="faded">
+
+  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+
+  <b-collapse is-nav id="nav_collapse">
+
+    <b-navbar-nav class="w-100" justified tabs>
+      <b-nav-item href="#">Apie Mus</b-nav-item>
+      <b-nav-item href="#">Kūrybinės Dirbtuvės</b-nav-item>
+      <b-nav-item href="#">Galerija</b-nav-item>
+      <b-nav-item href="#">Kontaktai</b-nav-item>
+    </b-navbar-nav>
+
+  </b-collapse>
+</b-navbar>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data () {
+    return {
+    }
+  }
+}
+</script>
